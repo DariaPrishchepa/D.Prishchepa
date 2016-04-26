1ee630e290be8a28a055cc53e73944cbff544394
Прищепа Дарья Владимировна <d.prishchepa@2gis.ru>
2016-04-26 16:12:21 +0600

    [Lec4]

---
+++ Lec4/Lec4/lec4/lec4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lec4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Telephone police = new Telephone("002");
+            Console.WriteLine(police.GetTelephone());
+        }
+    }
+}
---
+++ Lec4/Lec4/lec4/lec4/Telephone.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lec4
+{
+    class Telephone
+    {
+        readonly string Code;
+        readonly string Number;
+
+        public string GetTelephone()
+        {
+            return Code == string.Empty ? Number : $"({Code}){Number}";
+        }
+
+        public Telephone(string code, string number)
+        {
+            Number = number;
+            Code = code;
+        }
+
+        public Telephone( string number)
+        {
+            Number = number;
+        }
+    }
+}
